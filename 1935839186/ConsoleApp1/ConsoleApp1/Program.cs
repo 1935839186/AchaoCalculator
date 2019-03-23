1d71ecab6f77a51c6975a2137e0d747fce390763
1935839186 <1935839186@>
2019-03-23 19:02:03 +0800

    试试

--- 1935839186/ConsoleApp1/ConsoleApp1/Program.cs
+++ 1935839186/ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("你好啊");
+            Console.WriteLine("我很好啊");
         }
     }
 }
